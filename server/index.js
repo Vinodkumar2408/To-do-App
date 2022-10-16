514560e42cb481d4c17cee1d51cd279b72ba57c3
Vinodkumar2408 <113172273+Vinodkumar2408@users.noreply.github.com>
2022-10-16 10:56:10 +0530

    index1.html

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const path=require('path')
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'build')));
 
 const PORT = process.env.PORT || 5500;
 
@@ -20,6 +22,10 @@ mongoose.connect(process.env.DB_CONNECT)
 
 
 app.use('/', TodoItemRoute);
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'));
+})
 
 
 
